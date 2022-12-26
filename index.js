f6b7e45a1a0d3985a98fac1d0e47dbff594675e2
kathyayanikr <kathyayanikr2112001@gmail.com>
2022-12-26 19:38:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+//Question1
+
+let string="abcadeecfb";
+            let s=new Set();
+            for(let i=0;i< string.length;i++){
+                s.add(string[i]);
+            }
+
+            let result="";
+            for(let ch of s){
+                result=result+ch;
+            }
+            console.log(result);
+//Question2
+let str="abcadeecfb";
+let map=new Map();
+for(let i=0;i< str.length;i++){
+if(map.has(str[i])){
+let value=map.get(str[i]);
+    map.set(str[i],value+1);
+    }
+else{
+     map.set(str[i],1);
+    }
+}
+for(let [k,v] of map){
+    console.log(k+"="+v);
+} 
